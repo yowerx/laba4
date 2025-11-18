c6ae018a5876fae4d444918d4a04164aa8e23c16
Студент Студентов <is@athene.tech>
2025-11-18 09:15:15 +0300

    connected

---
+++ Source2.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main() {
+	int a = 4;
+	int b = 5;
+	printf("%d", a*b)
+}	
---
+++ Source3.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main() {
+	int a = 4;
+	int b = 5;
+	int c = 6;
+	printf("%d", a * b + 6)
+}
